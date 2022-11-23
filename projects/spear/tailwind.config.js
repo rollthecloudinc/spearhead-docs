af7ac54e5c02932c14364cda06aadd798ffb399d
druid <angular.druid@gmail.com>
2022-11-22 20:56:32 -0500

    Start replicating styling.

--- projects/spear/tailwind.config.js
+++ projects/spear/tailwind.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   content: [
+    "./projects/spear/src/**/*.{html,ts}",
     './projects/spear/src/assets/objects/media/*__classes.json',
     './projects/spear/src/assets/objects/panelpage/*.json'
   ],
